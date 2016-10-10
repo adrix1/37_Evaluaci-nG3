9e05a4a559a1f371616ecc22d8e54cac39e7e629
Adrix1 <adrixmalagon@gmail.com>
2016-10-10 16:28:30 -0500

    Segunda parte de la evaluación

---
+++ Ejercicios_2daparte/ejercicio4.js
@@ -0,0 +1,13 @@
+//EJERCICIO 4
+//Definir la siguiente jerarquía de objetos, haciendo uso de los prototipos de JavaScript:
+//Objeto Persona con las propiedades nombre, edad y género, y el método obtDetalles(), que muestra por pantalla las propiedades de la persona.
+//Objeto Estudiante, que hereda de Persona, e incluye las propiedades curso y grupo y el método registrar().
+//Objeto Profesor, que hereda de Persona, e incluye las propiedades asignatura y nivel y el método asignar().
+//Crear los objetos y casos de prueba necesarios para comprobar el correcto funcionamiento de la jerarquía.
+
+
+//No tengo idea de como solucionarlo...
+var nombre=prompt("Ingresa tu nombre");
+var edad=prompt("Ingresa tu edad");
+var genero=prompt("Ingresa tu nombre");
+alert("No tengo la repuesta");
