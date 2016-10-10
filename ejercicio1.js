6a02314e24c3e8223f216c26e2826e8ece518b37
Adrix1 <adrixmalagon@gmail.com>
2016-10-10 13:11:16 -0500

    Evaluación Sprint JS

---
+++ ejercicio1.js
@@ -0,0 +1,39 @@
+// Inicio
+// Primer turno Introduce piedra, papel o tijera.
+// Si primer turno es igual a piedra
+// Imprime empate
+// entonces segundo turno
+// Si primer turno es igual a papel
+// Imprime Ganaste primer turno
+// entonces segundo turno
+// Si primer turno es igual a tijera
+// Imprime Perdiste primer turno... Gana piedra
+// entonces segundo turno
+// Segundo turno Introduce piedra, papel o tijera.
+// Si segundo turno es igual a piedra
+// Imprime Perdiste segundo turno...
+// entonces tercer turno
+// Si segundo turno es igual a papel
+// Imprime empate...
+// entonces tercer turno
+// Si segundo turno es igual a tijera
+// Imprime Perdiste segundo turno...
+// entonces tercer turno
+// Tercer turno Introduce piedra, papel o tijera.
+// Si tercer turno es igual a piedra
+// Imprime Ganaste terce turno...
+//
+// Si tercer turno es igual a papel
+// Imprime Perdiste tercer turno...
+
+// Si tercer turno es igual a tijera
+// Imprime empate...
+
+
+
+var piedra=primer turno;
+var papel=segundo turno;
+var tijera=tercer turno;
+var ganas=ganaste;
+var pierdes=perdiste;
+var empate=empate;
---
+++ ejercicio2.js
@@ -0,0 +1,20 @@
+// Inicio
+// Introduce primernombre
+// leer nombre
+// primernombre=nombreUno
+// Introduce segundonombre
+// leer nombre
+// segundonombre=nombreDos
+// Introduce tercernombre
+// leer nombre
+// tercernombre=nombreTres
+
+var nombreUno = primernombre;
+var nombreDos = segundonombre;
+var nombreTres = tercernombre;
+
+var nombreUno = promt("Introduce primernombre");
+
+var nombreDos = promt("Introduce segundonombre");
+
+var nombreTres = promt("Introduce tercernombre");
